20d0b9968bd666082c7c6a8fb57721043f0a8dee
Surajit Sadhukhan <surajit@localhost.localdomain>
2017-08-17 23:44:23 +0530

    take picture from camera or gallery and send it to server

--- app/src/main/java/app/kotlinhelloworld/com/kotlinestart/myaccount.kt
+++ app/src/main/java/app/kotlinhelloworld/com/kotlinestart/myaccount.kt
@@ -17,6 +17,9 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import java.io.ByteArrayOutputStream
 import android.util.Base64
+import android.graphics.BitmapFactory
+
+
 class myaccount : AppCompatActivity() {
     var locationManager: LocationManager? = null
     private fun get_location()
@@ -33,7 +36,6 @@ class myaccount : AppCompatActivity() {
             Fuel.post(custom_url, listOf("todo" to "usermeta", "user" to user_name, "key" to "location", "value" to full_location)).responseString { request, response, result ->
                 result.fold({ d ->
                     val data: String = d.toString()
-                    //toast("service data - $data")
                     try
                     {
                         var service = JSONObject(data)
@@ -43,6 +45,10 @@ class myaccount : AppCompatActivity() {
                     finally
                     {
                         toast("location send successfully")
+                        val tsLong = System.currentTimeMillis() / 1000
+                        val ts = tsLong.toString()
+                        Fuel.post(custom_url, listOf("todo" to "usermeta", "user" to user_name, "key" to "last_update", "value" to "location,$ts")).responseString { request, response, result ->
+                        }
                     }
 
                 }, { err ->
@@ -60,38 +66,63 @@ class myaccount : AppCompatActivity() {
     }
 
     private fun encodeToBase64(image: Bitmap): String {
-        val baos = ByteArrayOutputStream()
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val b = baos.toByteArray()
+        val stm = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.PNG, 100, stm)
+        val b = stm.toByteArray()
         val imageEncoded = Base64.encodeToString(b, Base64.DEFAULT)
-        //toast("LOOK $imageEncoded")
         return imageEncoded
     }
+    private fun img_send_to_server(img_url: String)
+    {
+        var service_url = service_url()
+        var custom_url = service_url.service_url
+        toast("plz wait sending image to server")
+        Fuel.post(custom_url, listOf("todo" to "usermeta", "user" to user_name, "key" to "profile_pic", "value" to img_url)).responseString { request, response, result ->
+            result.fold({ d ->
+                val data: String = d.toString()
+                try
+                {
+                    var service = JSONObject(data)
+                    val service_status: String = service.getString("status")
+                    toast("service - $service_status")
+                }
+                finally
+                {
+                    toast("Image send successfully")
+                    val tsLong = System.currentTimeMillis() / 1000
+                    val ts = tsLong.toString()
+                    Fuel.post(custom_url, listOf("todo" to "usermeta", "user" to user_name, "key" to "last_update", "value" to "image,$ts")).responseString { request, response, result ->
+                    }
+                }
+
+            }, { err ->
+                toast("err --- " + err)
+            })
+        }
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+        if (requestCode == CAMERA_REQUEST  && resultCode == Activity.RESULT_OK) {
             val photo = data.extras.get("data") as Bitmap
             img_view.setImageBitmap(photo)
-            val base64_str: String = "data:image/jpeg;base64,"+encodeToBase64(photo)
-            var service_url = service_url()
-            var custom_url = service_url.service_url
-            toast("plz wait sending image to server")
-            Fuel.post(custom_url, listOf("todo" to "usermeta", "user" to user_name, "key" to "profile_pic", "value" to base64_str)).responseString { request, response, result ->
-                result.fold({ d ->
-                    val data: String = d.toString()
-                    try
-                    {
-                        var service = JSONObject(data)
-                        val service_status: String = service.getString("status")
-                        toast("service - $service_status")
-                    }
-                    finally
-                    {
-                        toast("Image send successfully")
-                    }
-
-                }, { err ->
-                    toast("err --- " + err)
-                })
+            val encodedImage: String = encodeToBase64(photo)
+            //val base64_str: String = "data:image/png;base64,$encodedImage"
+            //val decodedString = Base64.decode(encodedImage, Base64.DEFAULT)
+            //val decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+            //img_view_nxt.setImageBitmap(decodedByte)
+            img_send_to_server(encodedImage)
+        }
+        else if(requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM)
+        {
+            val targeted_uri = data.data
+            try {
+                val bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targeted_uri))
+                img_view_nxt.setImageBitmap(bitmap)
+                val encodedImage: String = encodeToBase64(bitmap)
+                img_send_to_server(encodedImage)
+            }
+            finally
+            {
+                toast("imaage show successfully")
             }
         }
     }
@@ -103,9 +134,7 @@ class myaccount : AppCompatActivity() {
     private fun selectImageInAlbum() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
-        }
+        startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -126,11 +155,9 @@ class myaccount : AppCompatActivity() {
             takepic_camera.setOnClickListener{
                 takePhoto()
             }
-            /*
             takepic_gallery.setOnClickListener {
                 selectImageInAlbum()
             }
-             */
             Fuel.get(user_activity_url).responseString { request, response, result ->
                 result.fold({ d ->
                     val data: String = d.toString()
